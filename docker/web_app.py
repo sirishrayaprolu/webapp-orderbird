2357a646a0cfe0be5b9149b735af4bd7ffd22bbd
6110373 <sirish.rayaprolu@thomsonreuters.com>
2022-06-18 21:56:07 +0530

    pushing the docker dir

---
+++ docker/web_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, send_from_directory
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+@app.route("/")
+def main():
+    #return 'Hello world'
+    return render_template('index.html')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5050)
