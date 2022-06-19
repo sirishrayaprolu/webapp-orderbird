acba0e313d35c27a51a6ba2578401a348f3dd560
6110373 <sirish.rayaprolu@thomsonreuters.com>
2022-06-19 12:36:54 +0530

    updating the paths in docker file

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python
+RUN pip install flask
+COPY ./docker/static /home/myapp/static/
+COPY ./docker/templates /home/myapp/templates/
+COPY web_app.py /home/myapp/
+EXPOSE 5050
+CMD python /home/myapp/web_app.py
--- run-docker.sh
+++ run-docker.sh
@@ -8,16 +8,16 @@ cp lambdas/web_app.py docker/.
 cp -r lambdas/templates/* docker/templates
 cp -r lambdas/static/* docker/static/.
 
-echo "FROM python" >> docker/Dockerfile
-echo "RUN pip install flask" >>docker/Dockerfile
-echo "COPY ./static /home/myapp/static/" >> docker/Dockerfile
-echo "COPY ./templates /home/myapp/templates/" >> docker/Dockerfile
-echo "COPY web_app.py /home/myapp/" >>docker/Dockerfile
-echo "EXPOSE 5050" >> docker/Dockerfile
-echo "CMD python /home/myapp/web_app.py" >> docker/Dockerfile
+echo "FROM python" >> Dockerfile
+echo "RUN pip install flask" >> Dockerfile
+echo "COPY ./docker/static /home/myapp/static/" >> Dockerfile
+echo "COPY ./docker/templates /home/myapp/templates/" >> Dockerfile
+echo "COPY web_app.py /home/myapp/" >> Dockerfile
+echo "EXPOSE 5050" >> Dockerfile
+echo "CMD python /home/myapp/web_app.py" >> Dockerfile
 
-cd docker
+#cd docker
 
-docker build -t orderbird-demo .
-docker run -t -d -p 5050:5050 --name orderbirdwebapp orderbird-demo
-docker ps -a
+#docker build -t orderbird-demo .
+#docker run -t -d -p 5050:5050 --name orderbirdwebapp orderbird-demo
+#docker ps -a
