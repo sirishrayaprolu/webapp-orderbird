eb07450bd24de2211b8cef020cc8e84fa21c2a21
6110373 <sirish.rayaprolu@thomsonreuters.com>
2022-06-19 12:41:52 +0530

    updating the paths in docker file

--- run-docker.sh
+++ run-docker.sh
@@ -12,7 +12,7 @@ echo "FROM python" >> Dockerfile
 echo "RUN pip install flask" >> Dockerfile
 echo "COPY ./docker/static /home/myapp/static/" >> Dockerfile
 echo "COPY ./docker/templates /home/myapp/templates/" >> Dockerfile
-echo "COPY web_app.py /home/myapp/" >> Dockerfile
+echo "COPY ./lambdas/web_app.py /home/myapp/" >> Dockerfile
 echo "EXPOSE 5050" >> Dockerfile
 echo "CMD python /home/myapp/web_app.py" >> Dockerfile
 
